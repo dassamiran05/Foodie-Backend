b2fc8eb89d8d3145cc38db07becace4cd26d534b
dassamiran05 <dassamiran05@gmail.com>
2024-02-18 19:20:44 +0530

    changes

--- multer/Imageupload.js
+++ multer/Imageupload.js
@@ -3,6 +3,7 @@
 
 import dotenv from "dotenv";
 import multer from "multer";
+import fs from "fs";
 
 // import cloudinary from require("../helper/cloudinaryconfig");
 // import moment from "moment";
@@ -12,7 +13,13 @@ dotenv.config();
 // img storage path
 const imgconfig = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, "../backend/uploads");
+    const uploadDir = "../uploads";
+
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+
+    callback(null, uploadDir);
   },
   filename: (req, file, callback) => {
     callback(null, `image-${Date.now()}.${file.originalname}`);
@@ -32,51 +39,3 @@ export const upload = multer({
   storage: imgconfig,
   fileFilter: isImage,
 });
-
-// user register
-// router.post("/register", upload.single("photo"), async (req, res) => {
-//   const upload = await cloudinary.uploader.upload(req.file.path);
-//   console.log(upload);
-
-//   const { name } = req.body;
-
-//   try {
-//     const date = moment(new Date()).format("YYYY-MM-DD");
-
-//     const userdata = new users({
-//       name: name,
-//       imgPath: upload.secure_url,
-//       date: date,
-//     });
-//     console.log(userdata);
-
-//     await userdata.save();
-//     res.status(200).send({ message: "Data added successfull", userdata });
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// });
-
-// user data get
-// router.get("/getdata", async (req, res) => {
-//   try {
-//     const getUser = await users.find();
-//     res.status(200).send(getUser);
-//   } catch (error) {
-//     res.status(400).json(error);
-//   }
-// });
-// router.delete("/deletedata/:id", async (req, res) => {
-//   try {
-//     await users.findByIdAndDelete(req.params.id);
-//     res.status(200).send({
-//       success: true,
-//       message: "Deleted successfully",
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send(error);
-//   }
-// });
-
-// module.exports = router;
